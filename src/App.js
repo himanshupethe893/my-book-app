c6ac969f653214fefc7848dcb02b868fc81c43ae
Hitman003 <himanshupethe08092003@gmail.com>
2023-12-15 22:37:52 +0530

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from your Express API
+    axios.get('http://localhost:3005/books')
+      .then(response => setBooks(response.data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Book List</h1>
+      <ul>
+        {books.map(book => (
+          <li key={book.book_id}>
+            <strong>{book.title}</strong> by {book.author_id}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
